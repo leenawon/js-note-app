3c877eee1c694ebc3e318be6a70468d693d6f9fd
leenawon <nawon5154@gmail.com>
2021-07-26 21:12:44 +0900

    note input 값 입력 확인 함수 추가 (checkNoteInput)

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -3,6 +3,8 @@ const addNoteButton = document.querySelector('.note-input button');
 const title = document.querySelector('.note-title');
 const content = document.querySelector('.note-content');
 
+const CHECK_CLASS = "check";
+
 let ID = 1;
 
 function Note(id, title, content) {
@@ -11,12 +13,33 @@ function Note(id, title, content) {
   this.content = content;
 }
 
+function checkNoteInput(title, content) {
+  const titleValue = title.value;
+  const contentValue = content.value;
+  if((titleValue !== "") && (contentValue !== "")) {
+    return true;
+  } else {
+    if(titleValue == "") {
+      title.classList.add(CHECK_CLASS);
+    }
+    if(contentValue == "") {
+      content.classList.add(CHECK_CLASS);
+    }
+  }
+  setTimeout(() => {
+    title.classList.remove(CHECK_CLASS);
+    content.classList.remove(CHECK_CLASS);
+  }, 1000);
+}
+
 function handleAddNote() {
+  if(checkNoteInput(title, content)) {
     let noteArray = [];
     let noteItem = new Note(ID, title.value, content.value);
     ID++;
     noteArray.push(noteItem);
     console.log(noteArray);
+  }
 }
 
 addNoteButton.addEventListener('click', handleAddNote);
